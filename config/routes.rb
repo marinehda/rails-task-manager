e2717615434e2e13a45704aec3c72a241686c9a1
Marine Houze <m.houzedelaulnoit@gmail.com>
2016-05-31 13:52:40 +0200

    exo1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  resources :tasks
+  # get '/tasks', to: 'tasks#index'
+
+  # get '/tasks/new', to: 'tasks#new'
+
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+  # post '/tasks', to: 'tasks#create'
+
+  # get '/tasks/:id/edit', to: 'tasks#edit'
+
+  # patch '/tasks/:id', to: 'tasks#update'
+
+  # get '/tasks/:id', to: 'tasks#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
